c0fb8e99bb14109c9c8916a6a42b271aef2fc7af
Pedro <nonameeee6666@gmail.com>
2023-03-22 12:17:28 -0300

    Eae pedrão

--- app/src/main/java/com/example/calculadora/Calculadora.java
+++ app/src/main/java/com/example/calculadora/Calculadora.java
@@ -10,5 +10,6 @@ public class Calculadora extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+        getSupportActionBar().hide();
     }
 }
