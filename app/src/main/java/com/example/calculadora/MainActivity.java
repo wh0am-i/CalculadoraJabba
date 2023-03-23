784e58c22de11788074bd91c00910f6015a87659
Pedro <nonameeee6666@gmail.com>
2023-03-23 09:49:35 -0300

    Tá funcionando?

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -5,23 +5,79 @@ import androidx.lifecycle.ViewTreeViewModelStoreOwner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     TextView resultado, contaAnterior;
-    String[] nomesIds = {"soma", "sub", "multi", "raiz",};
-    Button[] botoes = new Button[4];
+    int[] Ids = {R.id.raiz, R.id.potencia, R.id.CE, R.id.C, R.id.sete, R.id.oito, R.id.nove, R.id.multi, R.id.quatro, R.id.cinco, R.id.seis, R.id.sub, R.id.um, R.id.dois, R.id.tres, R.id.soma, R.id.mudaSinal, R.id.zero, R.id.virgula, R.id.divisao, R.id.calcular};
+    int[] nums = {R.id.zero, R.id.um, R.id.dois, R.id.tres, R.id.quatro, R.id.cinco, R.id.seis, R.id.sete, R.id.oito, R.id.nove}; //só pra diferenciar os números
+    Button[] botoes = new Button[21];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        for (int i = 0; i < 4; i++) {
-            int id = getResources().getIdentifier(nomesIds[i], "id", getPackageName());
-            botoes[i] = findViewById(id);
+        for (int i = 0; i <= 20; i++) {
+            botoes[i] = findViewById(Ids[i]);
+            for (int x = 0; x <= nums.length; x++) {
+                if (Ids[i] == nums[x]) {
+                    botoes[i].setOnClickListener(new View.OnClickListener() { //cria um listener para click
+                        @Override
+                        public void onClick(View view) { //chama o método
+                            numeros(nums[x]);
+                        }
+                    });
+                } else {
+                    botoes[i].setOnClickListener(new View.OnClickListener() { //cria um listener para click
+                        @Override
+                        public void onClick(View view) { //chama o método
+                            ((botoes[i]).toString()) ();
+                        }
+                    });
+                }
+            }
         }
     }
+
+    public void numeros(int num) {
+
+    }
+
+    public void raiz() {
+    }
+
+    public void potencia() {
+    }
+
+    public void CE() {
+    }
+
+    public void C() {
+    }
+
+    public void multi() {
+    }
+
+    public void sub() {
+    }
+
+    public void soma() {
+    }
+
+    public void mudaSinal() {
+    }
+
+    public void virgula() {
+    }
+
+    public void divisao() {
+
+    }
+
 }
